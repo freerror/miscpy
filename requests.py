0d58679cc9f93a0cded75db94e9808109f435700
appuser <appuser@fastmail.com>
2022-03-31 11:20:15 +1300

    Update requests.py

--- requests.py
+++ requests.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import json
 
 # Note, this was a PowerBI streaming dataset
-endpoint = r"https://api.powerbi.com/beta/68283f3b-8487-4c86-adb3-a5228f18b893/datasets/fd18a5b5-7e4b-47af-a964-991b54bedef8/rows?key=Xz4BC%2BwSz0ABG2rGvpXUy1AzucixGFiIn1wtFa9YyfuHy%2F9HXdfmwM1H4G8Nuyt5O%2FyMXxtyt2XeEv%2B9NGapnA%3D%3D"
+endpoint = r"https://api.powerbi.com/beta/asdfasdfasdf"
 date = datetime.strftime(
     datetime.now(),
     "%Y-%m-%dT%H:%M:%S"
